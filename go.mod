54af8dca6497a68293dfd735c21e950fd67247d7
unknown <farizree@gmail.com>
2026-02-18 17:12:50 +0700

    Test call from GitHub

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/farizree/test-lib.git
+
+go 1.22
---
+++ greeting/start.go
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package greeting
+
+import "fmt"
+
+func Hello(name string) string {
+	return fmt.Sprintf("Hello %s", name)
+}
